363115cc4b9a1909434a1bb25f6c2c059fefeddd
Zekun Li <zli203@uottawa.ca>
2019-09-27 19:53:37 -0400

    papers

---
+++ Code/extract_normal_traffic.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+path = './Dataset'
+normal_traffic = []
+files = []
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.csv' in file:
+            files.append(os.path.join(r, file))
+for f in files:
+    print(f)
+    #df = pd.read_csv("D:\\4900 Project\\Dataset\\DNS\\DrDoS_DNS_split\\DrDoS_DNS_1.csv", low_memory=False)
+    df = pd.read_csv(f, low_memory=False)
+    for packet in df.values:
+        if packet[87] == "BENIGN":
+            normal_traffic.append(packet)
+
+normal_df=pd.DataFrame(normal_traffic,columns=pd.columns)
